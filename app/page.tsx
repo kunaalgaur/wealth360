90143957f1b0faf5d5faccd42265851fa3296848
kunaalgaur <kunaalgaurr@gmail.com>
2023-10-11 14:48:45 +0530

    Integrated framer motion

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import styles from './page.module.css';
 import { Parallax } from 'react-parallax';
+import { motion } from 'framer-motion';
 
 const page = () => {
     return (
@@ -14,16 +15,24 @@ const page = () => {
                 bgStyle={{ filter: 'brightness(30%)' }}
                 strength={300}>
                 <div id={styles.container}>
-                    <span id={styles.heading}>
+                    <motion.span
+                        id={styles.heading}
+                        initial={{ opacity: 0, y: '100%' }}
+                        whileInView={{ opacity: 1, y: '0%' }}
+                        viewport={{ once: true }}>
                         Invest in mutual funds easily!
-                    </span>
-                    <span id={styles.text}>
+                    </motion.span>
+                    <motion.span
+                        id={styles.text}
+                        initial={{ opacity: 0, y: '100%' }}
+                        whileInView={{ opacity: 1, y: '0%' }}
+                        viewport={{ once: true }}>
                         Growing your investments can be a challenging endeavor.
                         However, having timely access to accurate market
                         intelligence and the assistance of a well-positioned and
                         competent team can significantly enhance your prospects
                         for success.
-                    </span>
+                    </motion.span>
                 </div>
             </Parallax>
         </div>

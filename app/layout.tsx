7f4ebef5135881a9f6db42c5f87cc1872690f268
kunaalgaur <kunaalgaurr@gmail.com>
2023-10-11 19:52:49 +0530

    minor bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,8 @@ import { NextFontWithVariable } from 'next/dist/compiled/@next/font';
 import Topbar from '@/components/shared/Topbar/Topbar';
 import Footer from '@/components/shared/Footer/Footer';
 import { Poppins } from 'next/font/google';
-import styles from './layout.module.css';
 import { Metadata } from 'next';
 import './globals.css';
-import { Parallax } from 'react-parallax';
-import { usePageLoading } from '@/hooks/usePageLoading';
-import PreLoader from '@/components/UI/PreLoader/PreLoader';
 
 // Declaring fonts configurations
 const poppins: NextFontWithVariable = Poppins({
@@ -23,7 +19,7 @@ const poppins: NextFontWithVariable = Poppins({
 // Declaring metadata configurations for SEO purpose
 const metadata: Metadata = {
     title: 'Wealth360',
-    description: '', //left blank for future inhancement(SEO)
+    description: 'Sample text', //left blank for future inhancement(SEO)
 };
 
 const AppLayout = ({
@@ -31,19 +27,12 @@ const AppLayout = ({
 }: {
     children: React.ReactNode;
 }): JSX.Element => {
-    const loading: boolean = usePageLoading();
     return (
         <html lang="en" suppressHydrationWarning={true}>
             <body className={poppins.className} suppressHydrationWarning={true}>
-                {loading ? (
-                    <PreLoader />
-                ) : (
-                    <>
-                        <Topbar />
-                        {children}
-                        <Footer />
-                    </>
-                )}
+                <Topbar />
+                {children}
+                <Footer />
             </body>
         </html>
     );
